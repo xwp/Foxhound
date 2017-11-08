9bc7a8804eae0b44d6fc999ea7bed39754a7e32b
Weston Ruter <weston@xwp.co>
2017-11-07 22:47:13 -0800

    Disable slug field and post creation in Customize Posts

--- inc/customizer.php
+++ inc/customizer.php
@@ -35,7 +35,7 @@ function _foxhound_filter_wp_api_nav_menu_items_workaround( $items ) {
 }
 
 /**
- * Prevent Customize Posts plugin from forcing preview URLs for post links since frontend router doesn't understand them.
+ * Prevent Customize Posts plugin from forcing preview URLs for post links (e.g. ?preview=true&p=123) since frontend router doesn't understand them yet.
  *
  * Note that this means that posts and pages cannot be added in the Customizer at the moment with Foxhound.
  *
@@ -53,6 +53,30 @@ function _foxhound_disable_customize_post_link_draft( $wp_customize ) {
 	}
 }
 add_action( 'customize_register', '_foxhound_disable_customize_post_link_draft' );
+
+/**
+ * Prevent Customize Posts from adding a slug field since post preview URLs aren't yet recognized.
+ *
+ * @see _foxhound_disable_customize_post_link_draft()
+ */
+function _foxhound_disable_customize_posts_slug_control() {
+	if ( wp_script_is( 'customize-post-section' ) ) {
+		wp_add_inline_script( 'customize-post-section', 'wp.customize.Posts.PostSection.prototype.addSlugControl = function() {};' );
+	}
+}
+add_action( 'customize_controls_enqueue_scripts', '_foxhound_disable_customize_posts_slug_control', 20 );
+
+/**
+ * Prevent users from being able to create posts in the Customizer via Customize Posts since post preview URLs aren't yet recognized.
+ *
+ * @see _foxhound_disable_customize_post_link_draft()
+ */
+function _foxhound_disable_customize_post_creation() {
+	if ( wp_script_is( 'customize-posts' ) ) {
+		wp_add_inline_script( 'customize-posts', sprintf( '_.each( _wpCustomizePostsExports.postTypes, function( postType ) { postType.current_user_can.create_posts = false; } )' ) );
+	}
+}
+add_action( 'customize_controls_enqueue_scripts', '_foxhound_disable_customize_post_creation', 20 );
 
 /**
  * Prevent Customize Posts from attempting to do selective refresh for post partials since theme is React-rendered.
